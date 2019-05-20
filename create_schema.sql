6b44d3a879548eea5c29259b0ab70ed4e056eb28
Matthew Sedaghatfar <sedaghatfar@gmail.com>
2019-05-20 17:19:42 -0400

    Create create_schema.sql

---
+++ create_schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE public.Accounts (
+    Id INT,
+    Name varchar(50),
+    Account_type varchar(50),
+    Created_at timestamp
+);
+
+CREATE TABLE public.Organizations (
+    Id INT,
+    Name varchar(50),
+    Created_at timestamptz
+);
+
+CREATE TABLE public.Jobs (
+    Id INT,
+    Account_id INT,
+    Organization_id INT,
+    Created_at timestamptz,
+    Status varchar(50),
+  	State_abb varchar(10),
+    Costs Numeric
+);
